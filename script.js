ef6389c8f8d00cb243580d3406e976a1527db56c
XueL6135 <146861517+XueL6135@users.noreply.github.com>
2023-10-03 11:56:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Xue lin"; // update the part BETWEEN the "quotes"
+let userName = "XueL6135"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
-    e.innerHTML = ownerName;
+    e.innerHTML = Xue Lin;
 });
 
-document.querySelector("#github").href = "http://github.com/" + userName;
-document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
+document.querySelector("#github").href = "http://github.com/" + XueL6135;
+document.querySelector("#fork").href = "http://github.com/" + XueL6135 + "/" + XueL6135 + ".github.io";
